57fb93d39d6ab8823f0dec99d9635a341de7d2c1
Jia Wang <wj2016@gmail.com>
2024-02-14 15:28:01 +0100

    add keydown and keyup event

--- lib/deep_game_web/live/game_live.ex
+++ lib/deep_game_web/live/game_live.ex
@@ -3,6 +3,19 @@ defmodule DeepGameWeb.GameLive do
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, username: "test")}
+    socket =
+      assign(socket,
+        username: "test",
+        game_state: :welcome,
+        game: nil
+      )
+
+    {:ok, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event(event, _unsigned_params, socket) do
+    IO.inspect(event, label: "KEY EVENT")
+    {:noreply, socket}
   end
 end
