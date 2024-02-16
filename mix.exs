a7975dfc94870419c59a0fa978438759e7641491
Jia Wang <wj2016@gmail.com>
2024-02-16 12:59:20 +0100

    add Nx

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule DeepGame.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:nx, "~> 0.6.4"}
     ]
   end
 
