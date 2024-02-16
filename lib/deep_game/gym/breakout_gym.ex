a5e4325f43c98b4f6c49b03d1a5126c40bba2294
Jia Wang <wj2016@gmail.com>
2024-02-16 20:50:27 +0100

    try to use defn

--- lib/deep_game/gym/breakout_gym.ex
+++ lib/deep_game/gym/breakout_gym.ex
@@ -17,22 +17,51 @@ defmodule DeepGame.Gym.BreakoutGym do
     Breakout.handle_input(game, action)
   end
 
+  # def get_observation(game) do
+  #   render_info = Breakout.render_info(game)
+
+  #   background()
+  #   |> put_paddle(render_info.paddle)
+  #   |> put_ball(render_info.ball)
+  #   |> create_nx()
+  # end
+
   def get_observation(game) do
     render_info = Breakout.render_info(game)
 
+    #    t = background()
+
+    # |> put_paddle(render_info.paddle)
+
+    #      |> put_ball(render_info.ball)
+    {{paddle_r, paddle_c}, paddle} = get_paddle(render_info.paddle)
+    {{ball_r, ball_c}, ball} = get_ball(render_info.ball)
+
+    render_nx({{paddle_r, paddle_c}, paddle}, {{ball_r, ball_c}, ball})
+  end
+
+  defn render_nx({{paddle_r, paddle_c}, paddle}, {{ball_r, ball_c}, ball}) do
     background()
-    |> put_paddle(render_info.paddle)
-    |> put_ball(render_info.ball)
+    |> Nx.put_slice([paddle_r, paddle_c], paddle)
+    |> Nx.put_slice([ball_r, ball_c], ball)
+    |> create_nx()
+  end
+
+  defn create_nx(t) do
+    t
     |> reverse_y()
     |> condense()
   end
 
-  defp background() do
+  defn background() do
     Nx.broadcast(0, {@screen_height, @screen_width})
   end
 
-  defp reverse_y(t) do
-    Nx.reverse(t, axes: [0])
+  defp get_paddle(paddle) do
+    row = round(paddle.y)
+    col = round(paddle.x - paddle.width / 2)
+    paddle_t = Nx.broadcast(128, {paddle.height, paddle.width})
+    {{row, col}, paddle_t}
   end
 
   defp put_paddle(background, paddle) do
@@ -40,6 +69,19 @@ defmodule DeepGame.Gym.BreakoutGym do
     col = round(paddle.x - paddle.width / 2)
     paddle_t = Nx.broadcast(128, {paddle.height, paddle.width})
     Nx.put_slice(background, [row, col], paddle_t)
+  end
+
+  defp get_ball(ball) do
+    row = round(ball.y)
+    col = round(ball.x - ball.width / 2)
+
+    ball_t =
+      Nx.multiply(
+        Nx.broadcast(200, {ball.height, ball.width}),
+        Utils.ball_gray_scales(round(ball.width / 2))
+      )
+
+    {{row, col}, ball_t}
   end
 
   defp put_ball(background, ball) do
@@ -53,6 +95,10 @@ defmodule DeepGame.Gym.BreakoutGym do
       )
 
     Nx.put_slice(background, [row, col], ball_t)
+  end
+
+  defn reverse_y(t) do
+    Nx.reverse(t, axes: [0])
   end
 
   defn condense(t) do
